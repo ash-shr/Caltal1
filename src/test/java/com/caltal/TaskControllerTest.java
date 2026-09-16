4eebabc6daa59ad9138fc045a267ff9115822ace
Ashr005 <as2733@sussex.ac.uk>
2026-09-16 20:39:42 +0100

    Mock and Mockito Fix

--- src/test/java/com/caltal/TaskControllerTest.java
+++ src/test/java/com/caltal/TaskControllerTest.java
@@ -5,7 +5,7 @@ import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -18,13 +18,15 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+
+
 @WebMvcTest(TaskController.class)
 class TaskControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
 
-    @MockitoBean
+    @MockitoBean 
     private TaskService service;
 
     @Test
